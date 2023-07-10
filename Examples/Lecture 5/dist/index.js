c19195d5d26cf0d8922fce98b2fd9277b17653d6
Khaldoun Halawani <kldonnn@gmail.com>
2023-07-10 19:29:41 +0300

    Session 6  Routing 1 [methods]

--- Examples/Lecture 5/dist/index.js
+++ Examples/Lecture 5/dist/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data/MOCK_DATA.js';
 const app = express();
 const port = 3000;
+// app.use(express.text()); // Just to read the body of requests
+app.use(express.json()); // Just to read the body of requests
 app.get('/students/all', (req, res) => {
     res.json(data);
 });
@@ -9,9 +11,18 @@ app.get('/students/passed', (request, response) => {
     const passedStudents = data.filter(std => std.mark >= 60);
     response.json({ students: passedStudents, total: passedStudents.length });
 });
-app.get('/bye', (req, res) => {
-    res.send("Bye Ahmad!");
-    res.json();
+app.get('/students', (req, res) => {
+    // console.log(req);
+    res.send(data);
+});
+app.post('/students', (req, res) => {
+    // console.log(req.headers);
+    // console.log(req.method);
+    // console.log("Request Body:");
+    // console.log(req.body);
+    const newStudent = req.body;
+    data.unshift(newStudent);
+    res.send("Thanks, New student added!");
 });
 app.listen(port, () => {
     console.log(`The app is listening on port ${port}`);
--- Examples/Lecture 5/index.ts
+++ Examples/Lecture 5/index.ts
@@ -4,6 +4,9 @@ import data from './data/MOCK_DATA.js';
 const app = express();
 const port = 3000;
 
+// app.use(express.text()); // Just to read the body of requests
+app.use(express.json()); // Just to read the body of requests
+
 app.get('/students/all', (req, res) => {
   res.json(data);
 });
@@ -13,9 +16,21 @@ app.get('/students/passed', (request, response) => {
   response.json({ students: passedStudents, total: passedStudents.length });
 });
 
-app.get('/bye', (req, res) => {
-  res.send("Bye Ahmad!");
-  res.json()
+app.get('/students', (req, res) => {
+  // console.log(req);
+  res.send(data);
+});
+
+app.post('/students', (req, res) => {
+  // console.log(req.headers);
+  // console.log(req.method);
+  // console.log("Request Body:");
+  // console.log(req.body);
+
+  const newStudent = req.body;
+  data.unshift(newStudent);
+
+  res.send("Thanks, New student added!");
 });
 
 app.listen(port, () => {
